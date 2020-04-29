bf9024e2e8c30113038f6dd216806fb28d163f46
BLake J <bjohnson293@ufl.edu>
2020-04-28 20:56:31 -0400

    euler angles now in degrees

--- madgwickFilter.c
+++ madgwickFilter.c
@@ -52,8 +52,6 @@ void imu_filter(float ax, float ay, float az, float gx, float gy, float gz){
     q_w = quat_mult(q_est_prev, q_w);        // equation (12)
     quat_scalar(&q_w, deltaT);               // equation (13) integrates the angles velocity to position
     quat_add(&q_w, q_w, q_est_prev);         // addition part of equation (13)
-    printf("q_w: ");                         // debug
-    printQuaternion(q_w);
     
     /* Compute the gradient by multiplying the jacobian matrix by the objective function. This is equation 20.
      The Jacobian matrix, J, is a 3x4 matrix of partial derivatives for each quaternion component in the x y z axes
@@ -66,8 +64,6 @@ void imu_filter(float ax, float ay, float az, float gx, float gy, float gz){
      */
     
     quat_Normalization(&q_a);              // normalize the acceleration quaternion to be a unit quaternion
-    printf("q_a normalized: ");
-    printQuaternion(q_a);
     //Compute the objective function for gravity, equation(15), simplified to equation (25) due to the 0's in the acceleration reference quaternion
     F_g[0] = 2*(q_est_prev.q2 * q_est_prev.q4 - q_est_prev.q1 * q_est_prev.q3) - q_a.q2;
     F_g[1] = 2*(q_est_prev.q1 * q_est_prev.q2 + q_est_prev.q3* q_est_prev.q4) - q_a.q3;
@@ -95,13 +91,9 @@ void imu_filter(float ax, float ay, float az, float gx, float gy, float gz){
     gradient.q3 = J_g[0][2] * F_g[0] + J_g[1][2] * F_g[1] + J_g[2][2] * F_g[2];
     gradient.q4 = J_g[0][3] * F_g[0] + J_g[1][3] * F_g[1] + J_g[2][3] * F_g[2];
     
-    printf("gradient: ");
-    printQuaternion(gradient);
-    
     // Normalize the gradient, equation (44)
     quat_Normalization(&gradient);
-    printf("gradient normalized: ");
-    printQuaternion(gradient);
+  
     /* This is the sensor fusion part of the algorithm.
      Combining Gyroscope position angles calculated in the beginning, with the quaternion orienting the accelerometer to gravity created above.
      Noticably this new quaternion has not be created yet, I have only calculated the gradient in equation (19).
@@ -117,10 +109,9 @@ void imu_filter(float ax, float ay, float az, float gx, float gy, float gz){
     quat_scalar(&gradient, (beta));             // multiply normalized gradient by beta
     quat_sub(&q_est_dot, q_w, gradient);        // subtract above from q_w, the integrated gyro quaternion
     quat_scalar(&q_est_dot, deltaT);
-    quat_add(&q_est, q_est_prev, q_est_dot);      // Integrate orientation rate to find position
-    printQuaternion(q_est);
+    quat_add(&q_est, q_est_prev, q_est_dot);     // Integrate orientation rate to find position
     quat_Normalization(&q_est);                 // normalize the orientation of the estimate
-    printQuaternion(q_est);                     //(shown in diagram, plus always use unit quaternions for orientation)
+                                                //(shown in diagram, plus always use unit quaternions for orientation)
    
 }
 
@@ -137,7 +128,10 @@ void eulerAngles(struct quaternion q, float* roll, float* pitch, float* yaw){
     *pitch = -asinf(2*q.q2*q.q4 + 2*q.q1*q.q3);                                  // equatino (8)
     *roll  = atan2f((2*q.q3*q.q4 - 2*q.q1*q.q2), (2*q.q1*q.q1 + 2*q.q4*q.q4 -1));
     
-   
+    *yaw *= (180.0f / PI);
+    *pitch *= (180.0f / PI);
+    *roll *= (180.0f / PI);
+
 }
 
 
--- main.c
+++ main.c
@@ -10,11 +10,27 @@
 #include "madgwickFilter.h"
 int main(int argc, const char * argv[]) {
     
+    
+    // flat upright position
     float roll = 0.0, pitch = 0.0, yaw = 0.0;
-    for(int i = 0; i<100; i++){
-        imu_filter(0, 0.5, 0.5, 0, 0, 0);
+    for(int i = 0; i<1000; i++){
+        imu_filter(0.05, 0.05, 0.9, 0, 0, 0);
         eulerAngles(q_est, &roll, &pitch, &yaw);
-        printf("roll: %f, pitch: %f, yaw: %f\n", roll, pitch, yaw);
+        printf("i: %d, roll: %f, pitch: %f, yaw: %f\n", i, roll, pitch, yaw);
+    }
+    
+    //45 deg roll with angular rotation
+    for(int i = 0; i<1000; i++){
+        imu_filter(0, -0.5, 0.5, -2, 0, 0);
+        eulerAngles(q_est, &roll, &pitch, &yaw);
+        printf("i: %d, roll: %f, pitch: %f, yaw: %f\n", i, roll, pitch, yaw);
+    }
+    
+    //-45 deg roll with angular rotation
+    for(int i = 0; i<1000; i++){
+        imu_filter(0, 0, 1, 2, 0, 0);
+        eulerAngles(q_est, &roll, &pitch, &yaw);
+        printf("i: %d, roll: %f, pitch: %f, yaw: %f\n", i, roll, pitch, yaw);
     }
     return 0;
 }
